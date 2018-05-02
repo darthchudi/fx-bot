314c34c4515787b15f322e06a488319f772705b2
enkaypeter <enkaypeter@gmail.com>
2018-05-02 06:31:38 +0100

    changing nginx config

--- vendor/heroku/heroku-buildpack-php/conf/nginx/default_include.conf.php
+++ vendor/heroku/heroku-buildpack-php/conf/nginx/default_include.conf.php
@@ -6,5 +6,8 @@ location / {
 
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.(json|lock|phar)$|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
-	deny all;
+	index  index.php index.html index.htm;
+	try_files $uri $uri/ /index.php?$query_string;
+
+	#deny all;
 }
